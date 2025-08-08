14ec95587a4452a382de15ec10d7eb6bb58f5144
Akaabiam Richards. <richardsakaabiam@gmail.com>
2025-08-09 00:51:04 +0300

    production

--- src/main/java/com/richards/achievepro/service/AchievementServiceImpl.java
+++ src/main/java/com/richards/achievepro/service/AchievementServiceImpl.java
@@ -56,7 +56,7 @@ public class AchievementServiceImpl implements AchievementServiceInterface {
                     existingAchievement.setTitle(requestDTO.getTitle());
                     existingAchievement.setDescription(requestDTO.getDescription());
                     existingAchievement.setRating(requestDTO.getRating());
-                    existingAchievement.setDateAchieved(String.valueOf(requestDTO.getDateAchieved()));
+                    existingAchievement.setDateAchieved((requestDTO.getDateAchieved()));
                     Achievement updatedAchievement = achievementRepository.save(existingAchievement);
                     return achievementMapper.toDto(updatedAchievement);
                 })
