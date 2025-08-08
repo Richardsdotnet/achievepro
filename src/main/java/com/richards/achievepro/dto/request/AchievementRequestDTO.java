97e855dc9c76915ac86f1e5f9623fec0149f6069
Akaabiam Richards. <richardsakaabiam@gmail.com>
2025-08-09 00:25:12 +0300

    production

--- src/main/java/com/richards/achievepro/dto/request/AchievementRequestDTO.java
+++ src/main/java/com/richards/achievepro/dto/request/AchievementRequestDTO.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 
 @Data
 @NoArgsConstructor
@@ -13,5 +15,5 @@ public class AchievementRequestDTO {
     private String title;
     private String description;
     private int rating; // Updated from 'score' to 'rating'
-    private String dateAchieved;
+    private LocalDate dateAchieved;
 }
